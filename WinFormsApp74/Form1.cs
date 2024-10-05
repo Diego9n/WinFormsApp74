9cda2804a239aabb66b5448aa243c78fad29313e
Diego9n <diegocarlos92.dc@gmail.com>
2024-10-05 00:52:29 -0300

    aaaa

--- WinFormsApp74/Form1.cs
+++ WinFormsApp74/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsApp74
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+        }
     }
 }
