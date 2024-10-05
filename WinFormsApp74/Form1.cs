c461003d21e6516b7407d4fd2a39f0b66108147d
Diego9n <diegocarlos92.dc@gmail.com>
2024-10-05 01:06:45 -0300

    button3

--- WinFormsApp74/Form1.cs
+++ WinFormsApp74/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormsApp74
         {
             int b;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int c;
+        }
     }
 }
